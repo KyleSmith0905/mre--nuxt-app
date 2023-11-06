57060906a980e9f933962529f1d0cbd465b825f9
kylesmith090502 <me@yskkyle.com>
2023-11-06 12:37:24 -0500

    Added auth secret.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   modules: ['@sidebase/nuxt-auth'],
   devtools: { enabled: true },
   ssr: true,
+  runtimeConfig: {
+    authSecret: process.env.AUTH_SECRET,
+  },
   auth: {
     baseURL: process.env.ORIGIN,
     globalAppMiddleware: false,
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -4,6 +4,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from '@prisma/client'
 
 export default NuxtAuthHandler({
+	secret: useRuntimeConfig().authSecret,
 	providers: [
 		// @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
 		GitHubProvider.default({
