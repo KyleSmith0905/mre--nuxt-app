cd2ae92a202bd7a7172a3c317b22a598e7eb7841
kylesmith090502 <me@yskkyle.com>
2023-11-06 12:33:18 -0500

    disabled global app middleware

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
   auth: {
-    baseURL: process.env.ORIGIN
+    baseURL: process.env.ORIGIN,
+    globalAppMiddleware: false,
   }
 })
