ea302ab08bd78b65d087a90c5ac0709afec96ed3
kylesmith090502 <me@yskkyle.com>
2023-11-06 21:58:06 -0500

    change an email (to trigger vercel deployment preview).

--- server/api/user.get.ts
+++ server/api/user.get.ts
@@ -4,7 +4,7 @@ export default defineEventHandler(async () => {
 	const prisma = new PrismaClient()
 
   console.log('Started query to find user');
-	const user = await prisma.user.findFirst({where: {email: 'not-an-example@example.com'}})
+	const user = await prisma.user.findFirst({where: {email: 'user@example.com'}})
   console.log('Finished query to find user');
 
   return {
