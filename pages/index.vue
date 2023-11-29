487614f2427fd334eebd1fd87395a7955cc6752f
kylesmith090502 <me@yskkyle.com>
2023-11-28 20:24:59 -0500

    pulled schema from database.

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@ const {signIn, signOut, status, lastRefreshedAt} = useAuth();
 </script>
 <template>
   <p>Auth Status: {{ status }}</p>
-  <p>Last Refreshed: {{ lastRefreshedAt?.toLocaleDateString() }}</p>
+  <p>Last Refreshed: {{ lastRefreshedAt?.toLocaleString() }}</p>
   <button @click="signIn('github')">Sign In</button>
   <button @click="signOut()">Sign Out</button>
 </template>
