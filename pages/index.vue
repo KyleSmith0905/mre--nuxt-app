5e3c1bbe2325d20e7c608217838cbb987eb9bb88
kylesmith090502 <me@yskkyle.com>
2023-11-28 20:14:26 -0500

    Changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-const {signIn, signOut, status} = useAuth();
+const {signIn, signOut, status, lastRefreshedAt} = useAuth();
 
 </script>
 <template>
   <p>Auth Status: {{ status }}</p>
+  <p>Last Refreshed: {{ lastRefreshedAt?.toLocaleDateString() }}</p>
   <button @click="signIn('github')">Sign In</button>
   <button @click="signOut()">Sign Out</button>
 </template>
